6f44ab00fad3e5d1c3d270ae86970dce9f2e6296
it-sayansamanta-3030 <sayansamanta2006@gmail.com>
2026-08-28 09:39:30 +0530

    Initial commit

---
+++ Prac2.py
@@ -0,0 +1,10 @@
+# Python program
+print("Program done by: Dhanali Samant")
+
+# Formula: area = 3.14 * r * r, p = 2 * 3.14 * r
+
+r = int(input("Enter radius of a circle: "))
+area = 3.14 * r * r
+p = 2 * 3.14 * r
+print("Area of a circle:", area)
+print("Perimeter of a circle:", p)
---
+++ Prac3.py
@@ -0,0 +1,17 @@
+# PYTHON PROGRAM
+print("Program by: Dhanali Samant")
+
+# Assign text to variable
+t1 = "Hello Learner!"
+print("Assigned value:")
+print(t1)
+
+# Get text from user
+t2 = input("Enter a text: ")
+print(t2)
+
+# Assign value/numbers to variable
+n1 = int(input("Enter first value: "))
+n2 = int(input("Enter second value: "))
+sum = n1 + n2
+print("Addition of two values:", sum)
